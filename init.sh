37c84967c0ca9b83e93c2bb52de55ea7b735b427
Zakharov Oleg <jsxft@hotmail.com>
2020-06-05 11:29:34 +0700

    add django app

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
+# run nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+# run gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn-hello.conf /etc/gunicorn.d/test-hello
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
